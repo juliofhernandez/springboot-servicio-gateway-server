e256e4814ba0f0a4ea777fde51c1721e2799c0d9
juliofhernandez <juliofernando1c@gmail.com>
2023-02-15 18:02:16 +0100

    EjemploGlobalFilter
    PRE y POST

---
+++ src/main/java/com/microservices/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -0,0 +1,38 @@
+package com.microservices.springboot.app.gateway.filters;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class EjemploGlobalFilter implements GlobalFilter {
+
+	private final Logger logger = LoggerFactory.getLogger(EjemploGlobalFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("Ejecutando Filtro PRE");
+		exchange.getRequest().mutate().headers(headers -> headers.add("token", "123123123"));
+		
+		return chain.filter(exchange).then(Mono.fromRunnable(()->{
+			logger.info("Ejecutando Filtro POST");
+			
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
+			
+			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+		}));
+	}
+
+}
