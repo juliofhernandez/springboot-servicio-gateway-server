4a804e40d72605dd3afa950ed2954cfb2b40f53f
juliofhernandez <juliofernando1c@gmail.com>
2024-12-26 17:00:05 +0100

    productsCircuitBreaker

--- src/main/java/com/microservices/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ src/main/java/com/microservices/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -17,21 +17,30 @@ import reactor.core.publisher.Mono;
 public class EjemploGlobalFilter implements GlobalFilter {
 
 	private final Logger logger = LoggerFactory.getLogger(EjemploGlobalFilter.class);
-	
+
+	/**
+	 * Este metodo define un filtro global en Spring Cloud Gateway que se ejecuta antes y después de que la solicitud sea procesada por el servicio backend.
+	 * En la fase PRE, se añade un encabezado `tokenRequest` a la solicitud.
+	 * En la fase POST, se agrega el encabezado `tokenResponse` a la respuesta y se añade una cookie `colorResponse`.
+	 *
+	 * @param exchange El objeto {@link ServerWebExchange} que contiene la información de la solicitud y la respuesta. Representa el contexto de la solicitud HTTP actual.
+	 * @param chain La cadena de filtros {@link GatewayFilterChain} que permite pasar la solicitud a través del siguiente filtro o servicio backend.
+	 * @return Un objeto {@link Mono<Void>} que representa el flujo asincrónico de la operación.
+	 * El filtro se ejecutará de forma no bloqueante. El metodo `then()` asegura que el código en la fase POST se ejecuta después de procesar la solicitud.
+	 */
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		logger.info("Ejecutando Filtro PRE");
-		exchange.getRequest().mutate().headers(headers -> headers.add("token", "123123123"));
+		logger.info("Ejecutando Filtro Global PRE");
+		exchange.getRequest().mutate().headers(headers -> headers.add("tokenRequestGlobal", "123123123"));
 		
 		return chain.filter(exchange).then(Mono.fromRunnable(()->{
-			logger.info("Ejecutando Filtro POST");
+			logger.info("Ejecutando Filtro Global POST");
 			
-			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
-				exchange.getResponse().getHeaders().add("token", valor);
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("tokenRequestGlobal")).ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("tokenResponseGlobal", valor);
 			});
 			
-			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
-//			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+			exchange.getResponse().getCookies().add("colorResponse", ResponseCookie.from("colorResponseGlobal", "rojo").build());
 		}));
 	}
 
--- src/main/java/com/microservices/springboot/app/gateway/filters/factory/EjemploGatewayFilterFactory.java
+++ src/main/java/com/microservices/springboot/app/gateway/filters/factory/EjemploGatewayFilterFactory.java
@@ -1,5 +1,6 @@
 package com.microservices.springboot.app.gateway.filters.factory;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -12,38 +13,49 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
-public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<EjemploGatewayFilterFactory.Configuracion>{
+public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<EjemploGatewayFilterFactory.ConfigurationCookie>{
 
 	private final Logger logger = LoggerFactory.getLogger(EjemploGatewayFilterFactory.class);
 
 	public EjemploGatewayFilterFactory() {
-		super(Configuracion.class);
+		super(ConfigurationCookie.class);
 	}
 
+	/**
+	 * Aplica el filtro definido en esta fábrica de filtros de puerta de enlace. El filtro realiza dos acciones:
+	 * 1. En la fase PRE, registra un mensaje de log que se obtiene desde la configuración y ejecuta código de filtrado.
+	 * 2. En la fase POST, agrega una cookie a la respuesta si se encuentra configurada en la clase {@link ConfigurationCookie}, y registra otro mensaje de log.
+	 *
+	 * @param config La configuración {@link ConfigurationCookie} que proporciona los valores de mensaje y cookie.
+	 * @return Un {@link GatewayFilter} que aplica el filtro PRE y POST según lo configurado.
+	 */
 	@Override
-	public GatewayFilter apply(Configuracion config) {
-		// Antes de chain.filter() es el PRE, y despues con el metodo .then() es el POST
+	public GatewayFilter apply(ConfigurationCookie config) {
 		return (exchange,chain)->{			
-			logger.info("Ejecutando PRE Gateway Filter Factory: "+ config.mensaje);			
+			logger.info("Ejecutando Gateway Filter Factory PRE: "+ config.mensajePRE);
+            // Gateway Filter Factory PRE Code
 			return chain.filter(exchange).then(Mono.fromRunnable(()->{
-				Optional.ofNullable(config.cookieValor).ifPresent(cookie -> {
-					exchange.getResponse().addCookie(ResponseCookie.from(config.cookieNombre, cookie).build());
-				});
-				logger.info("Ejecutando POST Gateway Filter Factory:" + config.mensaje);
+				Optional.ofNullable(config.cookieValor).ifPresent(cookie -> exchange.getResponse().addCookie(ResponseCookie.from(config.cookieNombre, cookie).build()));
+				logger.info("Ejecutando Gateway Filter Factory POST: " + config.mensajePOST);
 			}));
 		};
 	}
-	
-	public static class Configuracion {
-		private String mensaje;
+
+	/**
+	 * Clase interna que define los parámetros de configuración utilizados por el filtro de puerta de enlace.
+	 * Los valores configurados aquí son utilizados en la fase PRE (mensajes de log) y en la fase POST (valores de cookies).
+	 */
+	public static class ConfigurationCookie {
+		private String mensajePRE;
+		private String mensajePOST;
 		private String cookieValor;
 		private String cookieNombre;
 		
-		public String getMensaje() {
-			return mensaje;
+		public String getMensajePRE() {
+			return mensajePRE;
 		}
-		public void setMensaje(String mensaje) {
-			this.mensaje = mensaje;
+		public void setMensajePRE(String mensajePRE) {
+			this.mensajePRE = mensajePRE;
 		}
 		public String getCookieValor() {
 			return cookieValor;
@@ -56,7 +68,12 @@ public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<Ej
 		}
 		public void setCookieNombre(String cookieNombre) {
 			this.cookieNombre = cookieNombre;
-		}			
+		}
+		public String getMensajePOST() {
+			return mensajePOST;
+		}
+		public void setMensajePOST(String mensajePOST) {
+			this.mensajePOST = mensajePOST;
+		}
 	}
-
 }
